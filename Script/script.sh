1b683c5181d891b85913e6e5d9a5f152cf5630af
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2021-03-15 15:27:11 +0100

    Debugg script

--- Script/script.sh
+++ Script/script.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
+
+#-------------RECUPERATION DES DONNEES----------------
+
 #Sauvegarde de lancien IFS
 OLDIFS=$IFS
+pointeur=2
 
 #Recuperation des tous les noms des fichiers CSV
 fichiers=$(ls CSV)
-echo $fichiers
 
 for k in $fichiers;
 do
 	#Suppression de l'extension .csv
 	k=$( echo $k |cut -f1 -d '.')
 
+	#Ajout indications vu utilisateur
+	echo "Récupération des champs de l'index $k."
+
+	#recupération de tous les champs
 	header=$(head -n 1 CSV/$k.csv)
-	
+	tableauHeader=( ${header//,/ } )
+
+	#recupération de toutes les données du fichier
+	listData=$(tail +2 CSV/$k.csv)
+
 	attributs=""
 	IFS=,
 	#Recuperation de tous les champs d'un fichier
 	for i in $header;
 	do
-		attributs+="\"$i\" : {\"type\":\"string\"},";
-		
+		attributs+="\"$i\" : {\"type\":\"text\"},";
 	done
-	#DEBUGG////
-echo "--------------- ATTRIBUT --------------"
-echo "$attributs"
-	#////DEBUGG
+
+#Récupération de la ligne souhaité
+	IFS=$'\n';
+	#Ajout indications vu utilisateur
+	echo "Récupération des données à ajouter à l'index $k. Veuillez patienter, cette opération peut prendre quelques minutes."
+	for ligne in $listData;
+		do
+		data="{\"create\":{ \"_index\": \"$k\"}}{"
+		IFS=,
+		cmpt=0
+		for attribut in $ligne;
+		do
+			data+="\"${tableauHeader[${cmpt}]}\":\"$attribut\","
+			((cmpt=cmpt+1))
+		done
+		data=${data::-1}
+		data+=$'\n'
+		echo "$data}" >> data.json
+	done
 
 	#Suppression de la virgule a la fin de la variable
 	IFS=$OLDIFS
@@ -33,34 +58,32 @@ echo "$attributs"
 	echo "$attributs" >> attributs.json
 
 	#Création des properties par champs
-	tables="\"$k\" : { \"properties\" : { $(cat attributs.json)}},"
+	mappings="{\"mappings\": { \"properties\" : { $(cat attributs.json)}}}"
 	#Sauvegarde dans un fichier
-	echo "$tables" >> tables.json
-
-		#DEBUGG////
-	echo "-----------------TABLE------------"
-	echo "$tables"
-	#////DEBUGG
+	echo "$mappings" >> mappings.json
 
 	#Reinitialisation des attributs
 	rm attributs.json
 
-done
-
-#Suppression de la virgule a la fin du fichier
-echo "$(sed '$ s/.$//' "tables.json")" >> tablesok.json
-
-#Suppression du fichier tables.json qui devient inutile
-rm tables.json
-
-# Creation du mapping
-mapping="{\"mappings\": {$(cat tablesok.json)}}"
-#Sauvegarde du mapping dans un fichier pour pouvoir l'appeler dans la commande CURL
-echo "$mapping" >> mappings.json
+#-------------CREATION DE L'INDEX----------------
 
 #Creation de l'index de chaques fichiers
-curl -X PUT 'http://localhost:9200/test3?pretty' -H 'Content-Type: application/json' -d '@./mappings.json'
+
+#Ajout indications vu utilisateur
+echo "Création de l'index $k."
+
+curl -X PUT "http://localhost:9200/$k?pretty" -H 'Content-Type: application/json' -d '@./mappings.json'
+
+#-------------INSERTION DES DONNEES----------------
+#Ajout indications vu utilisateur
+echo "Insertion des données dans l'index $k."
+curl -X PUT "localhost:9200/$k/_bulk?&pretty" -H 'Content-Type: application/json' -d '@./data.json'
 
 #Suppression des fichiers inutiles
-#rm tablesok.json
-#rm mappings.json
+rm mappings.json
+rm data.json
+
+#Ajout indications vu utilisateur
+echo "Fin du script concernant l'index $k."
+
+done
