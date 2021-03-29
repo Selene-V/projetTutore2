35b6cc276ff859a63fd8a0eea42b874e74565c08
tatouvine <erwin.morel@laposte.net>
2021-03-29 14:13:47 +0200

    create componant Conected but you can't register because we have no API point

--- projet_tut/src/App.js
+++ projet_tut/src/App.js
@@ -2,21 +2,40 @@ import './App.css';
 import Home from "./Home/Home";
 import Detail from "./Detail/Detail";
 import Menu from "./Menu/Menu";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import Conection from "./Conection/Conection";
 
 function App() {
-    const [isClickForDetail, setIsClickForDetail] = useState("")
+    const [isClickForDetail, setIsClickForDetail] = useState("");
+    const [isConected, setIsConected] = useState(undefined);
+    const [onclickConection, setOnclickConection] = useState(false);
+
     useEffect(() => {
         setIsClickForDetail(isClickForDetail);
     });
+
+    console.log(onclickConection);
+
+    if (onclickConection) {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Conection setOnclickConection={setOnclickConection}/>
+                </header>
+            </div>
+        );
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <Menu setIsClickForDetail={setIsClickForDetail}/>
-                {isClickForDetail ? <Detail setIsClickForDetail={setIsClickForDetail} isClickForDetail={isClickForDetail}
-                                            /> :
+                <Menu setIsClickForDetail={setIsClickForDetail} isConected={isConected}
+                      setOnclickConection={setOnclickConection}/>
+                {isClickForDetail ?
+                    <Detail setIsClickForDetail={setIsClickForDetail} isClickForDetail={isClickForDetail} isConected={isConected}
+                    /> :
                     <Home setIsClickForDetail={setIsClickForDetail} isClickForDetail={isClickForDetail}
-                          />}
+                    />}
             </header>
         </div>
     );
--- projet_tut/src/Detail/Detail.tsx
+++ projet_tut/src/Detail/Detail.tsx
@@ -16,6 +16,7 @@ import Error from "../Error/Error";
 const Detail = (props: {
     setIsClickForDetail: any;
     isClickForDetail: any;
+    isConected: any;
 }) => {
     async function getValue() {
         return await fetch("http://projettutore2back/game/" + props.isClickForDetail)
@@ -143,17 +144,21 @@ const Detail = (props: {
                             </div>
                         </div>
                         <div className="w-1/12 text-center justify-center place-self-center text-white ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path
-                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
-                            </svg>
+                            {props.isConected !== undefined ?
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke="currentColor">
+                                        <path
+                                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                                    </svg>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd"
-                                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                                      clipRule="evenodd"/>
-                            </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd"
+                                              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                                              clipRule="evenodd"/>
+                                    </svg>
+                                </div> : ""}
+
                         </div>
                     </div>
                     <div className="text-left flex mt-20">
