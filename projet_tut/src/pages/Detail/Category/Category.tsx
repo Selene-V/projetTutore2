72c2fc4a9bb2512d0ae6b90feae5c097df09b7c3
tatouvine <erwin.morel@laposte.net>
2021-04-05 18:26:28 +0200

    add s for Category in page Detail

--- projet_tut/src/pages/Detail/Category/Category.tsx
+++ projet_tut/src/pages/Detail/Category/Category.tsx
@@ -4,7 +4,7 @@ const Category = (props: { categories: string[]; }) => {
     return (
         <div className="mt-3 lg:mt-0 xl:mt-0 bg-white rounded-xl lg:w-7/12 xl:w-7/12">
             <div className="bg-green-500 rounded-t-xl">
-                <p className="ml-2">Category</p>
+                <p className="ml-2">Categorys</p>
             </div>
             <div className="flex flex-wrap space-x-2 p-2 text-black">
                 {props.categories.map((value: string, index: number) =>
