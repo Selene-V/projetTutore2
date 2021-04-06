2879c9f94c5cdce69e682198692a7d75836aef04
Nicolas <nico.n.8-8@hotmail.fr>
2021-04-06 12:16:13 +0200

    design for table

--- projet_tut/src/componant/Table/Table.tsx
+++ projet_tut/src/componant/Table/Table.tsx
@@ -13,40 +13,40 @@ const Table = (props: {
         return (
             <div>
                 {props.tableInfo.map((value, index) =>
-                    <div key={index} className="bg-white rounded-lg flex space-x-2 w-11/12 mx-auto my-2 h-max">
-                        <div className="relative flex-none w-4/12">
-                            <img className="absolute inset-0 w-full h-full object-cover rounded-l-lg"
+                    <div key={index} className="bg-white rounded-lg xl:flex lg:flex lg:space-x-2 xl:space-x-2 w-11/12 mx-auto my-2 h-max">
+                        <div className="lg:relative lg:flex-none xl:w-4/12 lg:w-4/12">
+                            <img className="xl:absolute lg:absolute w-full h-full inset-0 xl:object-cover lg:object-cover"
                                  src={value.image.headerImage}/>
                         </div>
-                        <div className="text-left w-full px-5 py-3 space-y-4 w-6/12">
-                            <div className="flex justify-between">
+                        <div className="text-left w-full px-5 py-3 space-y-4">
+                            <div className="justify-center text-center lg:flex lg:justify-between xl:flex xl:justify-between">
                                 <div>{value.name}</div>
                             </div>
-                            <div className="flex justify-between">
-                                <div className="space-y-2 w-2/5 text-left">
+                            <div className="lg:flex lg:justify-between xl:flex xl:justify-between">
+                                <div className="space-y-2 lg:w-2/5 xl:w-2/5 text-left">
                                     <div className="flex space-x-3 text-base">
                                         <div className="w-1/3">{value.releaseDate}</div>
                                         <div
                                             className="2/3">{value.developer}</div>
                                     </div>
-                                    <div className="text-lg">Positive
+                                    <div className="text-lg text-center lg:text-left xl:text-left">Positive
                                         Review: {Math.round((parseInt(value.positiveRatings) / (parseInt(value.positiveRatings) + parseInt(value.negativeRatings))) * 100)}%
                                     </div>
                                     <div
-                                        className="text-2xl flex space-x-2">{value.platforms.map((result: string, secondIndex: number) =>
+                                        className="justify-center lg:jusitify-start xl:justify-start text-2xl flex space-x-2">{value.platforms.map((result: string, secondIndex: number) =>
                                         <div key={secondIndex}>
                                             <Platform platform={result}/>
                                         </div>
                                     )}</div>
                                 </div>
-                                <div className="text-base w-3/5">
+                                <div className="text-sm lg:text-base xl:text-base lg:w-3/5 xl:w-3/5">
                                     <div className="text-justify">
                                         <p>
                                             {value.description.shortDescription}
                                         </p>
                                         <button
                                             onClick={() => props.setIsClickForDetail(value.id)}
-                                            className="transition duration-500 float-right hover:bg-green-200 px-5 py-2 rounded-lg border-2 hover:border-opacity-0 border-gray-400">Look
+                                            className="transition duration-500 xl:float-right lg:float-right hover:bg-green-200 px-5 py-2 rounded-lg border-2 hover:border-opacity-0 border-gray-400">Look
                                         </button>
                                     </div>
                                 </div>
