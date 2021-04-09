fde2453a165a1a595207b5a4bba6975de048e2c4
tatouvine <erwin.morel@laposte.net>
2021-04-09 18:39:32 +0200

    menu text left

--- projet_tut/src/componant/Menu/Menu.tsx
+++ projet_tut/src/componant/Menu/Menu.tsx
@@ -5,10 +5,10 @@ const Menu = (props: { isConected: any; setOnclickConection: any; setIsConected:
     return (
         <nav className="bg-gray-800 w-full">
             <div className="mx-4">
-                <div className="relative flex items-center justify-end h-16 w-full">
+                <div className="relative flex items-center justify-end h-16 w-max">
                     <div
                         className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <div className="ml-3 relative">
+                        <div className="relative">
                             <div>
                                 {props.isConected === null ?
                                     <button className="bg-gray-500 p-2 flex text-sm rounded-lg text-lg font-medium text-gray-100
